c4680d28957b8a9e695b9e4fb185b243fbad3c55
Logan-Hardy <70916374+Logan-Hardy@users.noreply.github.com>
2021-04-12 14:11:43 -0600

    filtering

--- Intex2/Controllers/BurialCrudController.cs
+++ Intex2/Controllers/BurialCrudController.cs
@@ -300,8 +300,9 @@ namespace Intex2.Controllers
         }
 
         [HttpPost]
-        public IActionResult Filtered(BurialListViewModel filterAtr)
+        public IActionResult Filtered(BurialListViewModel filterAtr, int pageNum = 1)
         {
+            int pageSize = 20;
             string sex = filterAtr.FilterItems.Sex;
             string area = filterAtr.FilterItems.Area;
             double length = filterAtr.FilterItems.Length;
@@ -345,7 +346,23 @@ namespace Intex2.Controllers
                     .FromSqlInterpolated($"SELECT * FROM Burials WHERE Gender_Code LIKE {sex} AND Square LIKE {area} AND Burial_Direction LIKE {bdirection} AND North_or_South LIKE {nors} AND East_or_West LIKE {eorw} AND BurialID LIKE {burialid}")
                     .Where(b => b.LengthM >= length)
                     .Where(b => b.BurialDepth >= depth)
-                    .ToList()
+                    .OrderBy(b => b.BurialId)
+                    .Skip((pageNum - 1) * pageSize)
+                    .Take(pageSize)
+                    .ToList(),
+
+                PagingInfo = new PagingInfo
+                {
+                    ItemsPerPage = pageSize,
+                    CurrentPage = pageNum,
+                    TotalNumItems = _context.Burials
+                    .FromSqlInterpolated($"SELECT * FROM Burials WHERE Gender_Code LIKE {sex} AND Square LIKE {area} AND Burial_Direction LIKE {bdirection} AND North_or_South LIKE {nors} AND East_or_West LIKE {eorw} AND BurialID LIKE {burialid}")
+                    .Where(b => b.LengthM >= length)
+                    .Where(b => b.BurialDepth >= depth)
+                    //FOR THE PRESENTATION TO PRESENT CLEAN DATA .Where(x=> x.BurialSouthToFeet != null)
+                    .Count()
+                },
+                Photos = _context.Photos
             });
         }
 
