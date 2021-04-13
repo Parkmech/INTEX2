576d842c86db13b56334ef9a0a297fbfcd16a1bd
quinnschris <71037316+quinnschris@users.noreply.github.com>
2021-04-13 14:34:56 -0600

    changes to google
    changes to upload branch

--- Intex2/Controllers/GoogleController.cs
+++ Intex2/Controllers/GoogleController.cs
@@ -20,45 +20,6 @@ namespace Intex2.Controllers
         private SignInManager<IdentityUser> signInManager;
 
 
-        public class LoginModel : PageModel
-        {
-            private SignInManager<IdentityUser> signInManager;
-
-            public LoginModel(SignInManager<IdentityUser> signinMgr)
-            {
-                signInManager = signinMgr;
-            }
-
-            [BindProperty]
-            [Required]
-            public string UserName { get; set; }
-
-            [BindProperty]
-            [Required]
-            public string Password { get; set; }
-
-            [BindProperty(SupportsGet = true)]
-            public string ReturnUrl { get; set; }
-
-            public async Task<IActionResult> OnPostAsync()
-            {
-
-                if (ModelState.IsValid)
-                {
-                    Microsoft.AspNetCore.Identity.SignInResult result =
-                        await signInManager.PasswordSignInAsync(UserName, Password,
-                            false, false);
-                    if (result.Succeeded)
-                    {
-                        return Redirect(ReturnUrl ?? "/");
-                    }
-                    ModelState.AddModelError("", "Invalid username or password");
-                }
-
-                return Page();
-            }
-        }
-
     public GoogleController(UserManager<IdentityUser> userMgr, SignInManager<IdentityUser> signinMgr)
         {
             userManager = userMgr;
