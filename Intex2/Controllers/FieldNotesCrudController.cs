10e1fb35326fd73cc7d61d34d987a338a2dc6822
Logan-Hardy <70916374+Logan-Hardy@users.noreply.github.com>
2021-04-13 00:17:52 -0600

    merged with Parkers

--- Intex2/Controllers/FieldNotesCrudController.cs
+++ Intex2/Controllers/FieldNotesCrudController.cs
@@ -6,7 +6,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using Intex2.Models;
-using Intex2.Models.ViewdModels;
+using Intex2.Models.ViewModels;
 
 namespace Intex2.Controllers
 {
