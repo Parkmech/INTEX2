58b2e6b1be3daff84777c8444e8e881e65d0400d
Jeffrey Havell <havelljd@gmail.com>
2021-04-13 16:22:01 -0600

    About page

--- Intex2/Controllers/HomeController.cs
+++ Intex2/Controllers/HomeController.cs
@@ -27,6 +27,10 @@ namespace Intex2.Controllers
         {
             return View();
         }
+        public IActionResult About()
+        {
+            return View();
+        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
