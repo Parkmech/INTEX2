d1d87e023f7c55ac7baf41e76b218283844ee5d2
quinnschris <71037316+quinnschris@users.noreply.github.com>
2021-04-13 11:28:23 -0600

    Added changes to cookie policy

--- Intex2/Startup.cs
+++ Intex2/Startup.cs
@@ -13,6 +13,8 @@ using Intex2.Models;
 using Microsoft.EntityFrameworkCore;
 using Intex2.Services;
 using Amazon.S3;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Http;
 
 namespace Intex2
 {
@@ -28,6 +30,7 @@ namespace Intex2
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+
             services.AddControllersWithViews();
 
             services.AddSingleton<IS3Service, S3Service>();
@@ -42,7 +45,13 @@ namespace Intex2
                 opts.UseSqlServer(Configuration[
                     "ConnectionStrings:EgyptConnection"]));
 
-            services.AddAuthentication().AddGoogle(options =>
+
+            services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme).AddCookie(options =>
+            {
+                options.Cookie.SameSite = SameSiteMode.None;
+                options.Cookie.SecurePolicy = CookieSecurePolicy.Always;
+                options.Cookie.IsEssential = true;
+            }).AddGoogle(options =>
             {
                 options.ClientId = "545269694418-0aj3phfni9pv1mbpsstptmrbtg8qgj0l.apps.googleusercontent.com";
                 options.ClientSecret = "KI0GGc875Dw4uDUlepGFWAn-";
@@ -54,6 +63,13 @@ namespace Intex2
                 options.AddPolicy("RequireAdministratorRole", policy => policy.RequireRole("Admins"));
 
                 options.AddPolicy("RequireResearcherRole", policy => policy.RequireRole("Researcher"));
+            });
+
+            services.AddSession(options =>
+            {
+                options.Cookie.SameSite = SameSiteMode.None;
+                options.Cookie.SecurePolicy = CookieSecurePolicy.Always;
+                options.Cookie.IsEssential = true;
             });
 
             services.AddDbContext<EgyptContext>(opts =>
