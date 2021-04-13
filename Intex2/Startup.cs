a1a538879625f96671f80c769da14967f309a8b7
quinnschris <71037316+quinnschris@users.noreply.github.com>
2021-04-13 11:49:27 -0600

    Changes to forwarded headers

--- Intex2/Startup.cs
+++ Intex2/Startup.cs
@@ -106,7 +106,15 @@ namespace Intex2
             //    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
             //    app.UseHsts();
             //}
+
             app.UseHttpsRedirection();
+
+            app.UseForwardedHeaders(
+                new ForwardedHeadersOptions
+                {
+                    ForwardedHeaders = Microsoft.AspNetCore.HttpOverrides.ForwardedHeaders.XForwardedFor | Microsoft.AspNetCore.HttpOverrides.ForwardedHeaders.XForwardedProto
+                });
+
             app.UseStaticFiles();
 
             app.UseRouting();
